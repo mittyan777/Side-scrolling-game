5f1192fc043e79d772abf81c394643093d805a8d
mittyan777 <131222874+mittyan777@users.noreply.github.com>
2025-07-18 00:16:11 +0900

    エネミー処理（途中）

--- Assets/Script/Enemy1.cs
+++ Assets/Script/Enemy1.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class Enemy1 : MonoBehaviour
 {
+    private bool Direction = false;
+    SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position -= transform.right * 3 * Time.deltaTime;
+        if(Direction == false) { transform.position -= transform.right * 3 * Time.deltaTime; }
+        else { transform.position += transform.right * 3 * Time.deltaTime; }
+
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "bloc")
         {
+           if(Direction == false) { Direction = true; }
+            else {  Direction = false; }
 
+           if(spriteRenderer.flipX == false) { spriteRenderer.flipX = true; }
+            else {  spriteRenderer.flipX = false; }
+            
         }
     }
 }
---
+++ Assets/Script/test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    GameObject parentGameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+         parentGameObject = transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(parentGameObject.name);
+    }
+}
