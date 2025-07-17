621e4e0b3dfd1487453799add66b1edaf515ea1b
mittyan777 <131222874+mittyan777@users.noreply.github.com>
2025-07-17 10:09:50 +0900

    初期データ

---
+++ Assets/Player.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float Speed = 0;
+    [SerializeField] float Speed2 = 0;
+    [SerializeField] float jumpPower;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector3.up * jumpPower;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if(Input.GetKey("d"))
+        {
+            if (Speed < 5)
+            {
+                Speed += 0.1f;
+            }
+        }
+        else
+        {
+            if (Speed > 0)
+            {
+                Speed -= 0.1f;
+            }
+            else if (Speed < 0.01f)
+            {
+                Speed = 0;
+            }
+        }
+        if (Input.GetKey("a"))
+        {
+            if (Speed2 > -5)
+            {
+                Speed2 -= 0.1f;
+            }
+        }
+        else
+        {
+            if (Speed2 < 0)
+            {
+                Speed2 += 0.1f;
+            }
+            else if (Speed2 > 0.01f)
+            {
+                Speed2 = 0;
+            }
+        }
+
+        transform.position += transform.right * Speed * Time.deltaTime;
+        transform.position += transform.right * Speed2 * Time.deltaTime;
+    }
+}
